e3e25ae783836c7627e488634e38ead838653fdb
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-26 14:31:55 -0600

    added home controller and partial

---
+++ js/controllers/homeCtrl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log('homeCtrl loaded');
+
+app.controller('HomeCtrl', function($scope, DataFactory, AuthFactory, $routeParams, $location, $window) {
+  console.log("HomeCtrl");
+});
