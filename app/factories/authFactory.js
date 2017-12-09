b9b6283d3d22b7ebce1761c74ab6f166c26b3fd7
Joey Biotti <joey@MacBook-Pro-2.local>
2017-12-08 22:46:00 -0600

    added firebase/auth

--- app/factories/authFactory.js
+++ app/factories/authFactory.js
@@ -1,3 +1,69 @@
 "use strict";
 
 console.log("authFactory.js loaded");
+
+app.factory("AuthFactory", function(){
+
+    let currentUser = null;
+
+    let createUser = function(userObj){
+        return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
+        .catch(function(error){
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log('error', errorCode, errorMessage);
+        })
+    };
+
+    let loginUser = function(userObj) {
+        return firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
+        .catch( function(error){
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log('error', errorCode, errorMessage);
+        });
+    };
+
+    let logout = function(){
+        console.log('logout user');
+        return firebase.auth().signOut();
+    };
+
+    let isAuthenticated = function(){
+        console.log("AuthFactory: isAuthenticated");
+        return new Promise ((resolve, reject) => {
+            firebase.auth().onAuthStateChange((user) => {
+                if (user) {
+                    currentUser = user.uid;
+                    console.log('user', currentUser);
+                    resolve(true);
+                }else {
+                    resolve(false);
+                }
+            });
+        });
+    };
+
+    let getUser = function(){
+        return currentUser;
+    };
+
+    let provider = new firebase.auth.GoogleAuthProvider();
+
+    let authWithProvider = function(){
+        return firebase.auth().signInWithPopup(provider);
+    };
+
+    let user =  firebase.auth().currentUser;
+    let name, uid;
+
+    if(user !== null){
+        name = name.displayName;
+        uid = user.uid;
+    };
+
+    console.log('user name: ', name + 'user uid: ', uid);
+
+    return {createUser, loginUser, logout, isAuthenticated, getUser, authWithProvider};
+
+});
