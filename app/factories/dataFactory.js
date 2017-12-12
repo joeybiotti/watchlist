1199e102fb09723b5d978c2159fa0576e5e6e06a
Joey Biotti <joey@MacBook-Pro-2.local>
2017-12-12 00:52:01 -0600

    wrote delete movie function

--- app/factories/dataFactory.js
+++ app/factories/dataFactory.js
@@ -20,17 +20,33 @@ app.factor("DataFactory", function($q, $http, FBCreds, AuthFactory){
         });
     };
 
-        const editMovie = (movie, obj) => {
-            console.log('editing movie', movie, obj);
-            return $q((resolve, reject) =>{
-                $http.patch(`${FBCreds.databaseURL}/movies/${movie}.json`, JSON.stringify(obj))
-                .then((newObj) =>{
-                    resolve(newObj.data);
-                })
-                .catch((error) => {
-                    reject(error);
-                })
-            });
-        };
+    const editMovie = (movie, obj) => {
+        console.log('editing movie', movie, obj);
+        return $q((resolve, reject) =>{
+            $http.patch(`${FBCreds.databaseURL}/movies/${movie}.json`, JSON.stringify(obj))
+            .then((newObj) =>{
+                resolve(newObj.data);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+        });
+    };
 
-    });
+    const removeMovie = (movie) => {
+        console.log('removing movie', movie);
+        let movies = [];
+        return $q((resolve, reject) => {
+            $http.delete(`${FBCreds.databaseURL}/movies/${movie}.json`)
+            .then((userMovies) => {
+                resolve(movies);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+
+    return {addToWatchList, editMovie, removeMovie};
+
+});
