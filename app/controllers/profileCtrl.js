54339e12d7729b61676171c5c0a62e028152d71b
Joey Biotti <joey@MacBook-Pro-2.local>
2017-12-12 20:33:06 -0600

    wrote remove function

--- app/controllers/profileCtrl.js
+++ app/controllers/profileCtrl.js
@@ -10,14 +10,26 @@ app.controller('ProfileCtrl', function(AuthFactory, DataFactory, SearchTerm, $sc
 
     DataFactory.getUserWatchlist(user)
     .then((movies) =>{
-        $scope.movies = movies;
+        $scope.savedMovies = movies;
+        console.log("$scope.savedMovies", $scope.savedMovies);
+        console.log("movies", movies);
     });
 
     $scope.edit = (movie, toEdit) => {
-        console.log("EDIT", toEdit, movie);
+        console.log("edit movies", toEdit, movie);
         DataFactory.editMovie(movie, toEdit)
         .then((x) =>{
             console.log("updated list", x);
         });
+    };
+
+    $scope.remove = (movie) => {
+        console.log("$scope.savedMovies", $scope.savedMovies);
+        DataFactory.removeMovie(movie)
+        .then(() => {
+            $scope.savedMovies = newList;
+        });
+        console.log("$scope.savedMovies after click", $scope.savedMovies);
+        let newSavedMovies = $scope.savedMovies;
     };
 });
